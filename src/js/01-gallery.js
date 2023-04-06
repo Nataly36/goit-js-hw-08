87abbe7f006ae610e07d725862c32bdc225b85cf
Nataly36 <movienata25@gmail.com>
2023-04-06 23:20:46 +0300

    new
    new

---
+++ src/js/01-gallery.js
@@ -0,0 +1,28 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+const list = document.querySelector(".gallery");
+
+const markup = galleryItems.reduce(
+  (acc, { original, preview, description }) =>
+    (acc += `<li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`),
+  ''
+);
+
+list.insertAdjacentHTML('beforeend', markup);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
